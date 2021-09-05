59006c69e1f25a144a05deea264f4c892c0f9455
Leonardo Murça <leonardoamurca@gmail.com>
2021-09-05 20:17:53 -0300

    Add NetworkInfo

---
+++ app/src/main/java/com/riyaldi/moviecatalogue/utils/NetworkInfo.kt
@@ -0,0 +1,9 @@
+package com.riyaldi.moviecatalogue.utils
+
+import com.riyaldi.moviecatalogue.BuildConfig
+
+object NetworkInfo {
+    const val BASE_URL = BuildConfig.BASE_URL
+    const val IMAGE_URL= BuildConfig.IMAGE_URL
+    const val API_KEY = BuildConfig.API_KEY
+}
